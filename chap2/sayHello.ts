cde79302489575c255b57a066ae2d91dfbbca6eb
Onozuka Shunsuke <shunsuke.onozuka@gmail.com>
2023-03-17 21:30:16 +0900

    エラーパターンの追加

--- chap2/sayHello.ts
+++ chap2/sayHello.ts
@@ -4,3 +4,11 @@ function sayHello(firstName: string) {
 
 let firstName: string = "Shunsuke";
 sayHello(firstName);
+
+// let age: number = 36;
+// sayHello(age);
+// >型 'number' の引数を型 'string' のパラメーターに割り当てることはできません。
+
+// const message = "Hello!";
+// message();
+// >この式は呼び出し可能ではありません。  型 'String' には呼び出しシグネチャがありません。
